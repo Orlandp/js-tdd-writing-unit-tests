17d12c7f08ecd1bab2bdcd12e742d517f54c18e7
pknowledge <jamesblssings@gmail.com>
2025-07-23 04:59:11 +0300

    addtion test on src

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,12 @@
 // Your tests here
+import { pointsForWord }from "../utils";
+
+describe("pointsForWord", () => {
+    it("calculates the total point for a word (1 point per vowel, 2per constant)", () =>{
+
+    });
+
+    it("handles uppercase and lowercase input", () =>{
+        expect(pointsForWord("tEst")).toBe(7);
+    });
+});
